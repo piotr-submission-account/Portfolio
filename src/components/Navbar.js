64e10f3dad58318ca5dc7be9e360eaf360a7aa79
piotrs-smg <piotr.sieminski@swissmarketplace.group>
2024-02-17 17:31:13 +0100

    hid clients for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,7 +84,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/clients"
@@ -95,7 +95,7 @@ function NavBar() {
                 />{" "}
                 Clients
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item>
               <Nav.Link
