13994a55af1af6903e599838adb447baa0b79190
piotrs-smg <piotr.sieminski@swissmarketplace.group>
2024-02-19 07:34:57 +0100

    removed resume for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -97,7 +97,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item> */}
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/resume"
@@ -105,7 +105,7 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             {/* <Nav.Item>
               <Nav.Link
