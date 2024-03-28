abc9619f7cb62270b2a9aad5d11b7ca603b13a56
piotrs-smg <piotr.sieminski@swissmarketplace.group>
2024-03-28 13:30:08 +0100

    projects change

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,13 +15,13 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Being updated as you scroll through...
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "40px" }}>
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={astro}
               isBlog={false}
@@ -30,7 +30,7 @@ function Projects() {
               ghLink="https://gitlab.com/sieminskipiotr3/airflow-astr-dbt-deployment"
               // demoLink="https://chatify-49.web.app/"
             />
-          </Col>
+          </Col> */}
 
           {/* <Col md={4} className="project-card">
             <ProjectCard
